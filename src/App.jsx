c4aa440b9259135886882e35bef1540dc3e809eb
qamar-1214 <qshah9048@gmail.com>
2024-11-07 11:58:52 +0500

    home page hero section and transitions library

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
 import React from "react";
-import Background from "./components/Background";
-import Navbar from "./components/Navbar";
-import { Outlet } from "react-router-dom";
+import Background from "./components/Background/Background";
+import Navbar from "./components/Navbar/Navbar";
+import { Outlet, useLocation } from "react-router-dom";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 
 const App = () => {
+  const location = useLocation();
+
   return (
     <div className="relative">
       <Background />
       <div className="relative z-10 text-white">
         <Navbar />
-        <main className="min-h-[calc(100vh-120px)] flex items-center justify-center text-center">
-          <Outlet />
+        <main className="min-h-[calc(100vh-120px)]">
+          <TransitionGroup>
+            <CSSTransition key={location.key} classNames="page" timeout={500}>
+              <div>
+                <Outlet />
+              </div>
+            </CSSTransition>
+          </TransitionGroup>
         </main>
       </div>
     </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,19 @@
+// src/pages/Home.js
 import React from "react";
+import Hero from "../components/Home/Hero";
 
 const Home = () => {
-  return <div>Home page</div>;
+  return (
+    <div className="flex flex-col mt-2 md:mt-16  px-4 lg:px-8 pt-10 ">
+      {/* Hero Section */}
+      <Hero />
+
+      {/* Add more sections below, like About, Projects, etc. */}
+      <div className="mt-12">
+        Content for the rest of the home page goes here.
+      </div>
+    </div>
+  );
 };
 
 export default Home;
